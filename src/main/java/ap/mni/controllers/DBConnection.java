a69a32b7eb7f236000d233cd9abaad3160b5f60b
michael <azarrmickel@gmail.com>
2025-04-23 00:45:37 +0300

    Changing the IP from local host to my IP adress

--- src/main/java/ap/mni/controllers/DBConnection.java
+++ src/main/java/ap/mni/controllers/DBConnection.java
@@ -5,7 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/mvc_app";
+    private static final String URL = "jdbc:mysql://192.168.1.109:3306/mvc_app";
+
     private static final String USER = "root"; // change to your MySQL username if different
     private static final String PASSWORD = "123456"; // replace with your MySQL password
 
