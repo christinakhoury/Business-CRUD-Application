9061358eafde39861516dbda22a4e7e7df1b09f8
michael <azarrmickel@gmail.com>
2025-04-24 12:16:39 +0300

    changin IP

--- src/main/java/ap/mni/controllers/DBConnection.java
+++ src/main/java/ap/mni/controllers/DBConnection.java
@@ -5,10 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:mysql://192.168.1.109:3306/mvc_app";
-
-    private static final String USER = "root"; // change to your MySQL username if different
-    private static final String PASSWORD = "123456"; // replace with your MySQL password
+    private static final String URL = "jdbc:mysql://172.20.10.2:3306/mvc_app";
+    private static final String USER = "root"; // or "jimmy" if you're using that user
+    private static final String PASSWORD = "123456";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
