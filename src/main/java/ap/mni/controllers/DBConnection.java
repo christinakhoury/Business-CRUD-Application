23c29a6c69db232833019825292e7af92039c690
Christina Khoury <Christinakhoury20@gmail.com>
2025-05-05 11:01:38 +0300

    Added comments and updated StockController

--- src/main/java/ap/mni/controllers/DBConnection.java
+++ src/main/java/ap/mni/controllers/DBConnection.java
@@ -1,15 +1,20 @@
 package ap.mni.controllers;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.DriverManager;//Imports DriverManager, which is used to create a connection to a database using JDBC
 import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://172.20.10.2:3306/mvc_app";
-    private static final String USER = "root"; // or "jimmy" if you're using that user
+    private static final String USER = "root";
     private static final String PASSWORD = "123456";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
+/*ha to connect to your MySQL database using JDBC.
+
+Connection conn = DBConnection.getConnection();  use conn to create statements and interact with the database.
+
+ */
